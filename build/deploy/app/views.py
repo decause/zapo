93d9f9470391a6e43938d5e998b3bb1d536f5c05
luis <lrosario101@gmail.com>
2014-01-08 14:58:52 -0500

    updated views.py to render_template

--- build/deploy/app/views.py
+++ build/deploy/app/views.py
@@ -1,6 +1,31 @@
+from flask import render_template
 from app import app
 
+# index
 @app.route('/')
 @app.route('/index')
 def index():
-	return "hello world!"
+	return render_template('index.html',
+		description = 'zapotech // a digital development agency - based in albion, new york',
+		page = 'index')
+
+#about
+@app.route('/about')
+def about():
+	return render_template('about.html',
+		title = 'about',
+		page = 'about')
+
+#work
+@app.route('/work')
+def work():
+	return render_template('work.html',
+		title = 'work',
+		page = 'work')
+
+#contact
+@app.route('/contact')
+def contact():
+	return render_template('contact.html',
+		title = 'contact',
+		page = 'contact')
