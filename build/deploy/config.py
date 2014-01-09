19bd9dcf9bbb117a4c10de3e1a0796184be22546
luis <lrosario101@gmail.com>
2014-01-09 03:54:38 -0500

    added extra email settings

--- build/deploy/config.py
+++ build/deploy/config.py
@@ -4,6 +4,8 @@ SECRET_KEY = 'come-on-you-motherfucking-spurs'
 # mail server settings
 MAIL_SERVER = 'localhost'
 MAIL_PORT = 25
+MAIL_USE_TLS = False
+MAIL_USE_SSL = False
 MAIL_USERNAME = None
 MAIL_PASSWORD = None
 
