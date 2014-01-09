d5df396df92543a7a9a2d6e8febdf5f661c9fc4f
luis <lrosario101@gmail.com>
2014-01-09 01:15:11 -0500

    added 404 and 500 pages

--- build/deploy/app/views.py
+++ build/deploy/app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from forms import ContactForm
 
 # index
 @app.route('/')
@@ -24,8 +25,26 @@ def work():
 		page = 'work')
 
 #contact
-@app.route('/contact')
+@app.route('/contact', methods=('GET','POST'))
 def contact():
+	form = ContactForm()
+	if form.validate_on_submit():
+		return redirect('/')
 	return render_template('contact.html',
 		title = 'contact',
-		page = 'contact')
+		page = 'contact',
+		form = form)
+
+# 404
+@app.errorhandler(404)
+def internal_error(error):
+  return render_template('404.html',
+  	title='404',
+  	page='404'), 404
+
+# 500
+@app.errorhandler(500)
+def internal_error(error):
+	return render_template('500.html',
+		title='500',
+		page='500'), 500
