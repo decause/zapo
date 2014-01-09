ee8038b2ee37d93a346e82b9f1759dca53697dfc
luis <lrosario101@gmail.com>
2014-01-09 01:37:59 -0500

    added mail server base settings

--- build/deploy/config.py
+++ build/deploy/config.py
@@ -1,2 +1,11 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'come-on-you-motherfucking-spurs'
+
+# mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+# email list
+CONTACT_EMAIL = ['hello@zapo.tc']
