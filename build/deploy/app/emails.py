9d89bcaaff58608eb8c5d9cb031eac0e1e4a2bb4
luis <lrosario101@gmail.com>
2014-01-09 03:34:42 -0500

    added asynchronous email sending

--- build/deploy/app/emails.py
+++ build/deploy/app/emails.py
@@ -2,12 +2,17 @@ from flask.ext.mail import Message
 from flask import render_template
 from app import mail
 from config import CONTACT_EMAIL
+from threading import Thread
+
+def send_async_email(msg):
+	mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
 	msg = Message(subject, sender = sender, recipients = recipients)
 	msg.body = text_body
 	msg.html = html_body
-	mail.send(msg)
+	thr = Thread(target = send_async_email, args = [msg])
+	thr.start()
 
 def contact_email(form_content):
 	send_email("Contact Form Email",
